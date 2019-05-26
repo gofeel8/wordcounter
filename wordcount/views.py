c61435d8696c7a77adb39fa17d769e2b3b302426
gofeel8 <gofeel8@hanmail.net>
2019-05-27 00:57:39 +0900

    add func

--- wordcount/views.py
+++ wordcount/views.py
@@ -15,11 +15,16 @@ def result(request):
     text = request.GET['fulltext']
     words = text.split()
     word_dictionary = {}
-
+    digit = 0
+    alpha = 0
     for word in words:
+        if word.isdigit():
+            digit += 1
+        elif word.isalpha():
+            alpha += 1
         if word in word_dictionary:
             word_dictionary[word] += 1
         else:
             word_dictionary[word] = 1
 
-    return render(request, 'result.html', {'full': text, 'total': len(words), 'dictionary': word_dictionary.items()})
+    return render(request, 'result.html', {'full': text, 'total': len(words), 'dictionary': word_dictionary.items(), 'digit': digit, 'alpha': alpha})
